54e15e3be2c4c56866d1a857d0cf4bd28ea2f300
luismtns <dark.luismtns@gmail.com>
2018-08-13 17:21:59 -0300

    Atualizando

---
+++ Modulo3/TypeScript/base-ships.ts
@@ -0,0 +1,16 @@
+﻿class Spacecraft {
+
+    constructor (public propulsor: string){}
+    
+    jumpIntoHyperspace() {
+        console.log(`Entering hyperspace with ${this.propulsor}`)
+    }
+}
+
+interface Containership{
+
+    cargoContainers: number
+
+}
+
+export {Spacecraft, Containership}
