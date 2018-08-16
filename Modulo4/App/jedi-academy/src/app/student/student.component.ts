49bb6ea88b2c2ac7a95f97a994a42cff1ccad145
luismtns <dark.luismtns@gmail.com>
2018-08-16 11:54:34 -0300

    Secao 4, Aula 22

--- Modulo4/App/jedi-academy/src/app/student/student.component.ts
+++ Modulo4/App/jedi-academy/src/app/student/student.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
+  name: string = 'Luke'
+  isJedi: boolean = true
+
   constructor() { }
 
   ngOnInit() {
