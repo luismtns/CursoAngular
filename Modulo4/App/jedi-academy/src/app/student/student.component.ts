748456263311390bbaab17f2eb87abb2ab2822f3
luismtns <dark.luismtns@gmail.com>
2018-08-16 10:55:28 -0300

    Add

---
+++ Modulo4/App/jedi-academy/src/app/student/student.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
