e24d6d3b2f5d37b83e93d60df0bd3aa01bd106f1
luismtns <dark.luismtns@gmail.com>
2018-08-20 12:54:48 -0300

    Seção 4, aula 27

--- Modulo4/App/jedi-academy/src/app/app.component.ts
+++ Modulo4/App/jedi-academy/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import {Student} from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jedi-academy';
+
+  students: Student [] =[
+    {name: 'Luke', isJedi: true, temple: 'Corsucant'},
+    {name: 'Leai', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ]
+  
 }
